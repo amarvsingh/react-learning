e545e4c5c2c5862b28614e29ba748acdc5a55d4c
Amar Singh <amar.vs@somaiya.edu>
2024-01-30 10:03:50 +0530

    Initial Push with React Project Structure

---
+++ first-app/src/scripts/utils.js
@@ -0,0 +1,7 @@
+// Type Module needs to be very impprtant in script tag in HTML file for a Vanilla JS app
+
+//Default just needs to be one (gets caught automatically)
+export default "abcd-efg-hij";
+
+export let apiKey = "hdvvfc-123e3-adbad";
+export let timeout = 30;
